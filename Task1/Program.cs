a72f5ae8256a948d2cfeabca026d5341c5009e9e
SF-Roman <romasafr@gmail.com>
2021-10-01 19:53:29 +0300

    Commit Task 1-3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,39 @@ namespace Task1
 {
     class Program
     {
+        static PropertyInfo GetPropertyInfo(object obj, string str)
+        {
+            return obj.GetType().GetProperty(str);
+        }
+
+        static public void Print()
+        {
+            DateTime dateTime = new DateTime();
+
+            Console.WriteLine($"Attributes: {GetPropertyInfo(dateTime, "DayOfWeek").Attributes}");
+            Console.WriteLine($"CanRead: {GetPropertyInfo(dateTime, "DayOfWeek").CanRead}");
+            Console.WriteLine($"CanWrite: {GetPropertyInfo(dateTime, "DayOfWeek").CanWrite}");
+            Console.WriteLine($"CustomAttributes: {GetPropertyInfo(dateTime, "DayOfWeek").CustomAttributes}");
+            Console.WriteLine($"DeclaringType: {GetPropertyInfo(dateTime, "DayOfWeek").DeclaringType}");
+            Console.WriteLine($"IsSpecialName: {GetPropertyInfo(dateTime, "DayOfWeek").IsSpecialName}");
+            Console.WriteLine($"GetMethod: {GetPropertyInfo(dateTime, "DayOfWeek").GetMethod}");
+            Console.WriteLine($"GetValue: {GetPropertyInfo(dateTime, "DayOfWeek").GetValue(dateTime, null)}");
+            Console.WriteLine($"MemberType: {GetPropertyInfo(dateTime, "DayOfWeek").MemberType}");
+            Console.WriteLine($"MetadataToken: {GetPropertyInfo(dateTime, "DayOfWeek").MetadataToken}");
+            Console.WriteLine($"Module: {GetPropertyInfo(dateTime, "DayOfWeek").Module}");
+            Console.WriteLine($"Name: {GetPropertyInfo(dateTime, "DayOfWeek").Name}");
+            Console.WriteLine($"PropertyType: {GetPropertyInfo(dateTime, "DayOfWeek").PropertyType}");
+            Console.WriteLine($"ReflectedType: {GetPropertyInfo(dateTime, "DayOfWeek").ReflectedType}");
+            Console.WriteLine($"SetMethod: {GetPropertyInfo(dateTime, "DayOfWeek").SetMethod}");
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Это приложение выводит все свойства структуры DateTime.");
+            Console.WriteLine("Вывод будет осужествлен с помощью рефлексии\n");
+            Print();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task3/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        TrueFalse database;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void miNew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Add("123", true);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            };
+        }
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                tboxQuestion.Text = database[(int)nudNumber.Value - 1].Text;
+                cboxTrue.Checked = database[(int)nudNumber.Value - 1].TrueFalse;
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show($"Подробности: {ex.Message}", "Данный вопрос отсутствует");
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            database.Add((database.Count + 1).ToString(), true);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (nudNumber.Maximum == 1 || database == null) return;
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+        }
+        private void miSave_Click(object sender, EventArgs e)
+        {
+            if (database != null) database.Save();
+            else MessageBox.Show("База данных не создана");
+        }
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(ofd.FileName);
+                database.Load();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = database.Count;
+                nudNumber.Value = 1;
+            }
+        }
+        private void btnSaveQuest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                database[(int)nudNumber.Value - 1].Text = tboxQuestion.Text;
+                database[(int)nudNumber.Value - 1].TrueFalse = cboxTrue.Checked;
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show($"Подробности: {ex.Message}", "Откройте или создайте файл с вопросами");
+            }
+        }
+
+        private void imAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор: Safroshkin Roman");
+        }
+
+        private void imSaveAs_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // сохраняем текст в файл
+            if (database == null)
+            {
+                database = new TrueFalse(saveFileDialog1.FileName);
+                database.Add("Число", true);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+                MessageBox.Show("Файл сохранен");
+            }
+            else
+            {
+                database.FileName = saveFileDialog1.FileName;
+                database.Save();
+                MessageBox.Show("Файл сохранен");
+            }
+        }
+
+        private void tboxQuestion_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        }
+    }
+}
